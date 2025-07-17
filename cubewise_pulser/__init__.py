314c3153923b60c249cc6202240a12619613034d
JoeChow-HK <jchow@cubewise.com>
2025-07-17 17:07:36 +0800

    assertion for config.ini path

--- cubewise_pulser/__init__.py
+++ cubewise_pulser/__init__.py
@@ -1,6 +1,8 @@
+import os
 from configparser import ConfigParser
 
 from cubewise_pulser.config.cw_path import general_config_path
 
 config_ini = ConfigParser()
+assert os.path.exists(general_config_path), 'config path not found : {} '.format(general_config_path)
 config_ini.read(general_config_path)
